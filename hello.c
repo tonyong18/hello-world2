a7640eeffb16954234d792c54fda4d6382e8ee75
tonyong18 <40816655+tonyong18@users.noreply.github.com>
2018-08-30 16:39:20 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Tony, Justin
+ * Date: 2018/08/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Tony and Justin\n");
+  
+
+  return 0;
+}
